31a9c82e56dfa661b83224effe9a84bb30d5d00a
Savannah Sosa <savannah.e.sosa@gmail.com>
2022-07-26 13:35:19 -0400

    Initial Commit

---
+++ Shared/ApprenTic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApprenTic_Tac_ToeApp.swift
+//  Shared
+//
+//  Created by Savannah Sosa on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ApprenTic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
